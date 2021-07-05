396f395737abcbe7caf0e57b1c921828552020d2
askgenius <73232954+askgenius@users.noreply.github.com>
2021-07-05 17:20:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var feild, feildImage;
+var peashooter, peashooterImage;
+var zombiehead, zombieheadImage;
+var sunflower, sunflowerImage;
+var wallnut, wallnutImage;
+var zombie, zombieImage;
+var sun, sunImage;
+var pea, peaImage;
+
+function preload() {
+  feildImage = loadImage("feild.png")
+  peashooterImage = loadImage("peashooter.png")
+  zombieheadImage = loadImage("zombiehead.png")
+  sunflowerImage = loadImage("sunflower.png")
+  wallnutImage = loadImage("wallnut.png")
+  zombieImage = loadImage("zombie.png")
+  sunImage = loadImage("sun.png")
+  peaImage = loadImage("pea.png")
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
